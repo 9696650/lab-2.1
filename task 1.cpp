cc920d1fb818a5495b59326dde9b56eabef9fb09
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2025-11-18 22:34:45 +0300

    Create task 1.cpp

---
+++ task 1.cpp
@@ -0,0 +1,34 @@
+int main()
+{
+
+    int arr1[11];
+
+   
+    for (int i = 0; i < 11; i++)
+    {
+        if (i % 2 == 0)
+        {
+            arr1[i] = 6 * i - 9;
+        }
+        else
+        {
+            arr1[i] = -9 + 2 * i;
+        }
+    } 
+    int size = 11;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr1[j] > arr1[j + 1])
+            {
+                int temp = arr1[j];
+                arr1[j] = arr1[j + 1];
+                arr1[j + 1] = temp;
+            }
+        }
+    }
+
+
+    return 0;
+}
